96f4c66359fd61b459f3c83c7704cfd0edaf6a55
Hamza-Khamis <63231809+Hamza-Khamis@users.noreply.github.com>
2025-09-09 03:30:45 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-var typed = new Typed(".text", {
-    strings: ["IT Support Engineer", "Penetration Tester", "Red Teamer", "CTF Player", "Cybersecurity Engineer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
 
